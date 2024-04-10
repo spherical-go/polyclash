73a21ae6e9bbc5137ec7d4f3d934cc02bdad8aa4
Mingli Yuan <mingli.yuan@gmail.com>
2024-04-10 17:42:38 +0800

    only consider balanced case

--- polyclash/data/five_elements.py
+++ polyclash/data/five_elements.py
@@ -67,13 +67,6 @@ single_face_permutations = [
 counter = 0
 for charges in product(single_face_permutations, repeat=12):
     counter += 1
-    if counter % 100 == 0:
-        # give the percentage of completion
-        percentage = counter / 10**12 * 100
-        print(f"Progress: {percentage:.2f}% - Minimum energy: {min_energy} - Counter: {counter} - Energy: {energy}")
-        if percentage > 10:
-            break
-
     charges = np.stack(charges)
     if np.diff(charges, axis=1).sum() != 0:
         continue
@@ -81,6 +74,13 @@ for charges in product(single_face_permutations, repeat=12):
     if energy < min_energy:
         min_energy = energy
         optimal_charges = charges
+
+    if counter % 1000 == 0:
+        # give the percentage of completion
+        percentage = counter / 10**12 * 100
+        print(f"Progress: {percentage:.2f}% - Minimum energy: {min_energy} - Counter: {counter} - Energy: {energy}")
+        if percentage > 10:
+            break
 
 print("Optimal charges:", optimal_charges)
 print("Minimum potential energy:", min_energy)

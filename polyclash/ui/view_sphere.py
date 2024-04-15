fix rotation bug in map view

polyclash/ui/view_sphere.py
@@ -34,6 +34,7 @@ class ActiveSphereView(QtInteractor):
         super().__init__(parent)
         self.picker = None
         self.spheres = {}
+        self.cyclic_pad = 1
         self.initialize_interactor()
         self.status_bar = status_bar
         self.overlay_info = overlay_info
@@ -45,7 +46,6 @@ class ActiveSphereView(QtInteractor):
         self.show_axes = True
         self.add_axes(interactive=True)
         self.set_background("darkgray")
-
         self.add_mesh(mesh, show_edges=True, color="lightblue", pickable=False, scalars=face_colors, rgba=True)
 
         for idx, city in enumerate(cities):
@@ -54,8 +54,9 @@ class ActiveSphereView(QtInteractor):
             self.spheres[idx] = actor
 
         self.camera.position = 6 * axis[0]
-        self.camera.focal_point = axis[0]
-        self.camera.view_up = axis[1]
+        self.camera.focal_point = np.zeros((3,))
+        self.camera.view_up = axis[self.cyclic_pad]
+        self.update()
 
     def setup_scene(self):
         self.picker = self.interactor.GetRenderWindow().GetInteractor().CreateDefaultPicker()
@@ -97,27 +98,16 @@ class ActiveSphereView(QtInteractor):
         return
 
     def update_maps_view(self):
-        img0 = get_hidden().capture_view(0, 6 * axis[0], axis[0], axis[1])
-        self.overlay_map.set_image(0, 0, img0)
-        img1 = get_hidden().capture_view(1, 6 * axis[1], axis[1], axis[2])
-        self.overlay_map.set_image(1, 0, img1)
-        img2 = get_hidden().capture_view(2, 6 * axis[2], axis[2], axis[3])
-        self.overlay_map.set_image(2, 0, img2)
-        img3 = get_hidden().capture_view(3, 6 * axis[3], axis[3], axis[0])
-        self.overlay_map.set_image(3, 0, img3)
-        img4 = get_hidden().capture_view(4, 6 * axis[4], axis[4], axis[5])
-        self.overlay_map.set_image(0, 1, img4)
-        img5 = get_hidden().capture_view(5, 6 * axis[5], axis[5], axis[6])
-        self.overlay_map.set_image(1, 1, img5)
-        img6 = get_hidden().capture_view(6, 6 * axis[6], axis[6], axis[7])
-        self.overlay_map.set_image(2, 1, img6)
-        img7 = get_hidden().capture_view(7, 6 * axis[7], axis[7], axis[4])
-        self.overlay_map.set_image(3, 1, img7)
+        for row in range(self.overlay_map.rows):
+            for col in range(self.overlay_map.columns):
+                img = get_hidden().capture_view(6 * axis[row + self.overlay_map.rows * col], np.zeros((3,)),
+                                                axis[(row + self.cyclic_pad) % self.overlay_map.rows + self.overlay_map.rows * col])
+                self.overlay_map.set_image(row, col, img)
 
     def change_view(self, row, col):
-        self.camera.position = 6 * axis[row + 4 * col]
-        self.camera.focal_point = axis[row + 4 * col] * 0
-        self.camera.view_up = axis[(row + 1) % 4 + 4 * col]
+        self.camera.position = 6 * axis[row + self.overlay_map.rows * col]
+        self.camera.focal_point = np.zeros((3,))
+        self.camera.view_up = axis[(row + self.cyclic_pad) % self.overlay_map.rows + self.overlay_map.rows * col]
         self.update()
 
 
@@ -131,7 +121,7 @@ class PassiveSphereView(QtInteractor):
         self.set_background("darkgray")
         self.add_mesh(mesh, show_edges=True, color="lightblue", pickable=False, scalars=face_colors, rgba=True)
         for idx, city in enumerate(cities):
-            sphere = pv.Sphere(radius=0.02, center=city)
+            sphere = pv.Sphere(radius=0.03, center=city)
             actor = self.add_mesh(sphere, color=stone_empty_color, pickable=True)
             self.spheres[idx] = actor
         self.update()
@@ -156,9 +146,9 @@ class PassiveSphereView(QtInteractor):
         actor.GetProperty().SetColor(stone_empty_color[0], stone_empty_color[1], stone_empty_color[2])
         self.update()
 
-    def capture_view(self, ix, camera_position, camera_focus, camera_up):
+    def capture_view(self, camera_position, camera_focus, camera_up):
         self.camera.position = camera_position
-        self.camera.focal_point = camera_focus
+        self.camera.focal_point = camera_focus * 0
         self.camera.view_up = camera_up
         self.update()
         img = self.screenshot(transparent_background=True, scale=2, return_img=True)